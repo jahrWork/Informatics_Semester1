a4b42f9067d458e11f01c82120423761d9e2b5b3
Juan Hernandez <jahramos@gmail.com>
2024-09-12 00:22:08 +0200

    check

--- Python Semester 1 week by week/week15.py
+++ Python Semester 1 week by week/week15.py
@@ -11,19 +11,19 @@ from random import random, uniform
 # y = f(x) function to plot
 # y_i = f(x_i) forall i in [0, N]
 # **********************************************
-# N = 100
+N = 100
 
 
-# t = array([2*pi*i/N for i in range(N+1)])
-# print("t[N] =", t[N], 2*pi)
+t = array([2*pi*i/N for i in range(N+1)])
+print("t[N] =", t[N], 2*pi)
 
 
-# x = 3 * sin(t)  # x_i = 3 sin ( t_i )
-# y = 2 * cos(t)
+x = 3 * sin(t)  # x_i = 3 sin ( t_i )
+y = 2 * cos(t)
 
 
-# plt.plot(y, x, "blue")
-# plt.show()
+plt.plot(y, x, "blue")
+plt.show()
 
 # plt.plot(t, x,  "blue")
 # plt.plot(t, y,  "green")
